073ff2d31cd70c93606e5080be6bbc8b0b93bb16
Rakesh <krakesh7788@gmail.com>
2024-09-25 17:09:48 +0530

    comment gpt api

--- backend/controllers/openai.js
+++ backend/controllers/openai.js
@@ -1,23 +1,23 @@
-const OpenAI = require("openai")
+// const OpenAI = require("openai")
 
-const openai = new OpenAI({ apiKey: process.env.API_KEY })
+// const openai = new OpenAI({ apiKey: process.env.API_KEY })
 
-async function chatgpt(req, res) {
-  const { topic, questionCount, level } = req.body
-  const input = `Generate a ${topic} quiz with ${questionCount} questions of ${level} level. Each question should have a question, multiple-choice options. In format of {quiz_name, questions, answers}. A separate answers array contains the correct option number numbered from 1 to 4. Give only json data not any explanation`
+// async function chatgpt(req, res) {
+//   const { topic, questionCount, level } = req.body
+//   const input = `Generate a ${topic} quiz with ${questionCount} questions of ${level} level. Each question should have a question, multiple-choice options. In format of {quiz_name, questions, answers}. A separate answers array contains the correct option number numbered from 1 to 4. Give only json data not any explanation`
 
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "user", content: `${input}` }],
-    model: "gpt-3.5-turbo",
-  })
+//   const completion = await openai.chat.completions.create({
+//     messages: [{ role: "user", content: `${input}` }],
+//     model: "gpt-3.5-turbo",
+//   })
 
-  const formattedJsonString = JSON.stringify(
-    JSON.parse(completion.choices[0].message.content),
-    null,
-    2
-  )
-  console.log(formattedJsonString)
-  res.send(formattedJsonString)
-}
+//   const formattedJsonString = JSON.stringify(
+//     JSON.parse(completion.choices[0].message.content),
+//     null,
+//     2
+//   )
+//   console.log(formattedJsonString)
+//   res.send(formattedJsonString)
+// }
 
-module.exports = { chatgpt }
+// module.exports = { chatgpt }
